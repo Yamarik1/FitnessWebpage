d5cafef84f803076d7340ef41c00f491d948b5ce
Yamarik1 <yams.aj@gmail.com>
2025-04-11 16:09:43 -0400

    Created gradient Animations

--- hello/static/hello/home.js
+++ hello/static/hello/home.js
@@ -6,21 +6,29 @@ function apply_hover() {
     const linkBox = document.getElementById("fileLink")
     const linkBoxHover = document.getElementById("fileLinkHover")
     linkBox.addEventListener('mouseover', function (event) {
-        console.log("here")
-        let id = null
-        const elem = document.getElementById("line")
-        let pos = linkBox.getBoundingClientRect().top
-        clearInterval(id)
-        id = setInterval(frame, 1)
-        function frame() {
-            if (pos == 270) {
-                clearInterval(id)
-            } else {
-                pos++
-                elem.style.top = pos + "px";
+        const element = document.getElementById(event.target.id)
+        element.classList.add('cAnimate')
 
-            }
-        }
+
+        // const element = document.getElementById("line")
+        // element.style.animation = "none"
+        // element.style.animation = "link_animation 2s linear"
+        // console.log("here")
+
+        // let id = null
+        // const elem = document.getElementById("line")
+        // let pos = linkBox.getBoundingClientRect().top
+        // clearInterval(id)
+        // id = setInterval(frame, 1)
+        // function frame() {
+        //     if (pos == 270) {
+        //         clearInterval(id)
+        //     } else {
+        //         pos++
+        //         elem.style.top = pos + "px";
+
+        //     }
+        // }
 
         // const currElement = document.getElementById("fileLinkHover")
         // currElement.classList.remove("hidden")
@@ -28,13 +36,18 @@ function apply_hover() {
         // linkBox.classList.remove("file_link")
         // linkBox.classList.add("hidden")
     })
-
-    linkBoxHover.addEventListener('mouseout', function (event) {
-        const currElement = document.getElementById("fileLink")
-        currElement.classList.remove("hidden")
-        currElement.classList.add("file_link")
-
-        linkBoxHover.classList.remove("file_link")
-        linkBoxHover.classList.add("hidden")
+    linkBox.addEventListener('mouseout', function (event) {
+        const element = document.getElementById(event.target.id)
+        element.classList.remove('cAnimate')
     })
+
+
+    // linkBoxHover.addEventListener('mouseout', function (event) {
+    //     const currElement = document.getElementById("fileLink")
+    //     currElement.classList.remove("hidden")
+    //     currElement.classList.add("file_link")
+
+    //     linkBoxHover.classList.remove("file_link")
+    //     linkBoxHover.classList.add("hidden")
+    // })
 }
